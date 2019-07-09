cd4a513938f06ca30952142716b7b664024dbae3
Kevin Dickey <kdickey.run@gmail.com>
2019-07-08 23:18:54 -0600

    fixed typo

--- src/main/java/com/kevindickey/kdb/connections/PostgreSqlConnectionProvider.java
+++ src/main/java/com/kevindickey/kdb/connections/PostgreSqlConnectionProvider.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class PostgresSqlConnectionProvider implements ConnectionProvider {
+public class PostgreSqlConnectionProvider implements ConnectionProvider {
 
     private Connection connection;
 
--- src/test/java/com/kevindickey/kdb/PostgreSqlTests.java
+++ src/test/java/com/kevindickey/kdb/PostgreSqlTests.java
@@ -2,7 +2,7 @@ package com.kevindickey.kdb;
 
 import java.sql.SQLException;
 
-import com.kevindickey.kdb.connections.PostgresSqlConnectionProvider;
+import com.kevindickey.kdb.connections.PostgreSqlConnectionProvider;
 import com.kevindickey.kdb.sql.PostgreSqlProvider;
 import com.kevindickey.kdb.tables.CompanyTable;
 import com.kevindickey.kdb.tables.CompanyTableRow;
@@ -16,7 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostgreSqlTests {
-	private static PostgresSqlConnectionProvider connectionProvider;
+	private static PostgreSqlConnectionProvider connectionProvider;
 	private static PostgreSqlProvider sqlProvider;
 
 	@Test
@@ -44,7 +44,7 @@ public class PostgreSqlTests {
 	@BeforeClass
 	public static void connect() throws ClassNotFoundException, SQLException {
 		PostgreSqlTests.sqlProvider = new PostgreSqlProvider();
-		PostgreSqlTests.connectionProvider = new PostgresSqlConnectionProvider();
+		PostgreSqlTests.connectionProvider = new PostgreSqlConnectionProvider();
 		connectionProvider.connect("jdbc:postgresql://localhost:5432/postgres", "postgres", "password123");
 	}
 }
